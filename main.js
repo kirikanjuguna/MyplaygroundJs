21818292fc2a160065928d1bf38b8ba17eedf3f6
Test <njugunakirika@gmail.com>
2024-11-26 13:23:44 +0300

    house lighting

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 function kujaribu1() {
     document.getElementById("kujaribu").innerHTML = "The beautiful Journey begins";
 }
+
+function day(){
+    document.getElementById("micasa").innerHTML.src="images/3d-rendering-cartoon-house (1).gif";
+}
+
+function night(){
+    document.getElementById("micasa").innerHTML.src="images/3d-rendering-cartoon-house.gif";
+}
+
+function head() {
+    document.getElementsById("head").innerHTML = "My beautiful house";
+}
