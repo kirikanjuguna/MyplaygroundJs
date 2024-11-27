e49ccd51f06f699b6529f6ff186f8af77b711bec
Test <njugunakirika@gmail.com>
2024-11-27 12:19:15 +0300

    corrected typo causing error on js

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ function kujaribu1() {
     document.getElementById("kujaribu").innerHTML = "The beautiful Journey begins";
 }
 
+function headi() {
+    document.getElementById("head").innerHTML = "My beautiful house";
+}
+
+
 function day() {
     document.getElementById("house-img").innerHTML.src="images/3d-rendering-cartoon-house (1).gif";
 }
@@ -10,9 +15,5 @@ function night() {
     document.getElementById("house-img").innerHTML.src="images/3d-rendering-cartoon-house.gif";
 }
 //spent a day looking for what the error could be only to find out that the id i was using was for the whole div
-
-function head() {
-    document.getElementsById("head").innerHTML = "My beautiful house";
-}
 
 
