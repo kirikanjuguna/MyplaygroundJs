530220c852f2a91c56864149115293ec88d26c36
Edd <njugunakirika@gmail.com>
2026-05-24 13:05:14 +0300

    camel case & params

--- plain.js
+++ plain.js
@@ -1,28 +1,28 @@
 //1.Declare a variable for your full name using the correct keyword. It should never change.
-const FullName = "Edwin Kirika Njuguna";
+const fullName = "Edwin Kirika Njuguna";
 
 //2.Declare a variable for your age using the correct keyword. It can change on your birthday.
-let Age = 25;
+let age = 25;
 //3.Declare a variable called isEmployed that is currently false.
 let isEmployed = false;
 //4.Use typeof to log the type of all three variables to the console.
-typeof FullName
-typeof Age;
+typeof fullName
+typeof age;
 typeof isEmployed;
 
-console.log(typeof FullName);
-console.log(typeof Age);
-console.log(typeof isEmployed)
+console.log(typeof fullName);
+console.log(typeof age);
+console.log(typeof isEmployed);
 
 //5.Write a function called introduce that logs a sentence using all three variables — e.g. "Hi, I'm Edwin, I'm 25, employed: false".
 function introduce () {
- console.log("Hi, i'm" + FullName + "i'm" + Age + " and i'm" + isEmployed  )
+ console.log("Hi, i'm" + fullName + "i'm" + age + " and i'm" + isEmployed  )
 };
 
 //6.Inside that function, declare a variable called secret. Try to log secret outside the function. What happens and why?
 function introduce () {
     const secret = "i'm relearning JavaScript"
- console.log("Hi, i'm" + FullName + "i'm" + Age + " and i'm" + isEmployed  )
+ console.log(`Hi, i'm ${fullName}, i'm ${age}, and i'm ${isEmployed}`);
 };
 
 console.log(secret);
@@ -32,3 +32,14 @@ console.log(secret);
 
 console.log("5" + 1);//here the answer is gonna be 51 because the string comes first so a concatenation is gonna be carried out
 console.log(5 - "1");//here the answer is gonna be 4 because the string is gonna be converted to a number
+
+//Declare two number variables. Write a function that takes those two numbers as parameters, adds them, and returns the result. Log the result. Then try passing a string as one of the arguments — what does JavaScript do?
+ //lemme just try this without help
+ const x = 2
+ const y = 3
+  function add(x,y){
+    return(x + y);
+  };
+  console.log(add(x,y));
+  console.log(add(x,"10"));
+
